388fc6e3dad17b9e0285ab976bd7f81f04b82423
mukhlis <mukhlis@fisip.net>
2023-02-16 14:48:17 +0700

    add reporter

---
+++ master/log/reporter.ts
@@ -0,0 +1,78 @@
+
+
+import { useGlobalReturn } from 'esoftplay';
+import { LibCurl } from 'esoftplay/cache/lib/curl/import';
+import { LibObject } from 'esoftplay/cache/lib/object/import';
+import { LibProgress } from 'esoftplay/cache/lib/progress/import';
+import { UserClass } from 'esoftplay/cache/user/class/import';
+import useGlobalState from 'esoftplay/global';
+import Storage from 'esoftplay/storage';
+import * as FileSystem from 'expo-file-system';
+
+const state = useGlobalState<any[]>([], { persistKey: 'log/reporter', inFile: true, jsonBeautify: true })
+const isHasAccess = useGlobalState(false, { persistKey: 'log/reporter_access' })
+export default class m {
+
+  static getAccess(): void {
+    new LibCurl('user_reporter', null, (res, msg) => {
+      isHasAccess.set(Number(res?.send) == 0 ? false : true)
+    }, (err) => { }, 1)
+  }
+
+  static accessState(): useGlobalReturn<boolean> {
+    return isHasAccess
+  }
+
+  static dataState(): useGlobalReturn<any> {
+    return state
+  }
+
+  static addLog(url: string, post: string, _response: string): void {
+    if (isHasAccess.get()) {
+      var response = typeof _response == 'string' && ((_response.startsWith("{") && _response.endsWith("}")) || (_response.startsWith("[") && _response.endsWith("]"))) ? JSON.parse(_response) : _response
+      state.set(LibObject.push(state.get(), { url, post, response })())
+    }
+  }
+
+  static reset(): void {
+    isHasAccess.set(false)
+    state.reset()
+    Storage.removeItem('log/reporter')
+  }
+
+  static sendReport(cb?: () => void): void {
+    setTimeout(async () => {
+      const email = UserClass.state().get().email
+      const fileUri = Storage.getDBPath('log/reporter')
+
+      LibProgress.show('Mengirim report..')
+      try {
+        const fileInfo = await FileSystem.getInfoAsync(fileUri, {});
+        const fileName = fileInfo?.uri?.split('/').pop();
+        const formData = new FormData();
+        formData.append('caption', '#report from ' + email);
+        formData.append('chat_id', '-1001737180019');
+        formData.append('document', {
+          uri: fileUri,
+          name: email + '-' + fileName,
+          type: 'text/csv',
+        });
+        const response = await fetch(
+          `https://api.telegram.org/bot923808407:AAEFBlllQNKCEn8E66fwEzCj5vs9qGwVGT4/sendDocument`,
+          {
+            method: 'POST',
+            body: formData,
+          }
+        );
+        const result = await response.json();
+        this.reset()
+        cb?.()
+        LibProgress.hide()
+      } catch (error) {
+        LibProgress.hide()
+      }
+    }, 0);
+  };
+}
+
+
---
+++ master/log/reporter_menu.tsx
@@ -0,0 +1,41 @@
+// withHooks
+// noPage
+
+import { LibDialog } from 'esoftplay/cache/lib/dialog/import';
+import { LogReporter } from 'esoftplay/cache/log/reporter/import';
+import React from 'react';
+import { Pressable, Text, View } from 'react-native';
+
+
+export interface LogReporter_menuArgs {
+
+}
+export interface LogReporter_menuProps {
+
+}
+export default function m(props: LogReporter_menuProps): any {
+  const logCount = LogReporter?.dataState?.().useSelector(t => t)
+
+  return (
+    <View style={{ flexDirection: 'row', backgroundColor: '#222', }}>
+      <View style={{ padding: 10, flex: 1 }} >
+        <Text style={{ fontSize: 12, color: '#fff', fontWeight: 'bold', marginBottom: 10 }}>Anda dalam mode [REPORTER]</Text>
+        <Text style={{ fontSize: 12, color: '#fff', marginBottom: 10 }}>Silahkan akses halaman yang tidak sesuai, lalu kirimkan log ke developer, kami akan mengumpulkan data log untuk keperluan pengecekan.</Text>
+        <View style={{ flexDirection: 'row' }} >
+          <View style={{ flexWrap: 'wrap', paddingHorizontal: 10, height: 24, borderRadius: 12, backgroundColor: '#cce5ff', alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ color: '#004085', fontSize: 10 }}>{logCount.length} data terkumpul</Text>
+          </View>
+        </View>
+      </View>
+      <View style={{ justifyContent: 'flex-end', margin: 10 }} >
+        <Pressable onPress={() => {
+          LibDialog.warningConfirm('Konfirmasi', 'Yakin untuk mengirimkan data log sekarang? setelah log dikirim, mode REPORTER akan dimatikan.', 'Kirim', () => {
+            LogReporter.sendReport()
+          }, 'Batal', () => { })
+        }} style={{ padding: 8, backgroundColor: 'rgba(255,255,255,0.9)', marginTop: 10, borderRadius: 5 }}>
+          <Text style={{ color: 'red' }}>KIRIM</Text>
+        </Pressable>
+      </View>
+    </View>
+  )
+}
