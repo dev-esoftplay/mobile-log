d5f9e3f3760fcf8657d86f28fba33cbd51ef0a0c
kholil <kholil@fisip.net>
2024-07-24 08:31:52 +0700

    fix

--- master/mover.js
+++ master/mover.js
@@ -5,7 +5,7 @@ const espPath = '../esoftplay/'
 if (fs.existsSync(espPath + '/modules/log')) {
 	if (fs.existsSync(espPath + "modules/lib/curl.ts")) {
 		let curl = fs.readFileSync(espPath + "modules/lib/curl.ts", { encoding: 'utf8' })
-		// curl = curl.replace(`//api_logger_import`, `import { LogStateProperty } from 'esoftplay/cache/log/state/import';`)
+		curl = curl.replace(`//api_logger_import`, ``)
 		curl = curl.replaceAll(`//api_logger`, `if (esp.modProp("log/state")) {
           var resJson = typeof resText == 'string' && ((resText.startsWith("{") && resText.endsWith("}")) || (resText.startsWith("[") && resText.endsWith("]"))) ? JSON.parse(resText) : resText
           esp.modProp("log/state").doLogCurl(this.uri, this.url, post, this.isSecure, resJson, esp.mod("user/routes").getCurrentRouteName())
