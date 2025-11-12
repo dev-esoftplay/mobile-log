ca3ea8a764dcf1af9cf38854cd6e1f94d7c2c617
mukhlis <mukhlis@fisip.net>
2025-11-12 10:45:05 +0700

    add send result

--- master/log/detail.tsx
+++ master/log/detail.tsx
@@ -1,6 +1,5 @@
 // withHooks
 import { applyStyle } from 'esoftplay';
-import { LibCurl } from 'esoftplay/cache/lib/curl/import';
 import { LibIcon } from 'esoftplay/cache/lib/icon/import';
 import { LibNavigation } from 'esoftplay/cache/lib/navigation/import';
 import { LibProgress } from 'esoftplay/cache/lib/progress/import';
@@ -9,6 +8,7 @@ import { LibStatusbar } from 'esoftplay/cache/lib/statusbar/import';
 import { LibStyle } from 'esoftplay/cache/lib/style/import';
 import { LibTextstyle } from 'esoftplay/cache/lib/textstyle/import';
 import esp from 'esoftplay/esp';
+import Storage from 'esoftplay/storage';
 
 import React from 'react';
 import { Pressable, ScrollView, Text, View } from 'react-native';
@@ -23,33 +23,36 @@ export interface LogDetailProps {
 export default function m(props: LogDetailProps): any {
   const data = LibNavigation.getArgs(props, 'data')
 
-  function sendMsg() {
+  const sendToTelegram = () => {
     let config = esp.config()
-    let msg = [
+    const api = Object.keys(data)[0]
+
+    let notes: string[] = [
+      '#debugger_result',
       'slug: ' + '#' + esp?.appjson()?.expo?.slug,
       'domain: ' + config?.url.replace(/^https?:\/\//, ''),
-      'api:\n',
-      '`' + '`',
-      '\n',
-      '```',
+      'api: ' + api,
+    ]
 
-      '```'
-    ].join('\n')
+    let msg: string[] = []
+    msg.push(
+      JSON.stringify(data, undefined, 2)
+    )
 
-    let post = {
-      text: msg,
-      chat_id: '-610603314',
-      parse_mode: 'Markdown',
-      disable_web_page_preview: true
-    }
-    LibProgress.show()
-    new LibCurl().custom('https://api.telegram.org/bot923808407:AAEFBlllQNKCEn8E66fwEzCj5vs9qGwVGT4/sendMessage', post, () => LibProgress.hide())
+    LibProgress.show('Sedang mengirim result API ke Telegram..')
+    Storage.setItem('log_result_api', msg.join('\n')).then((v) => {
+      Storage.sendTelegram('log_result_api', notes.join('\n'), () => {
+        LibProgress.hide()
+      }, () => {
+        LibProgress.hide()
+      }, '-1001737180019', () => api)
+    })
   }
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, backgroundColor: '#2c3e50' }}>
       <LibStatusbar style='dark' />
-      <View style={applyStyle({ paddingTop: LibStyle.STATUSBAR_HEIGHT, borderBottomWidth: 0.5, borderBottomColor: "#ddd", flexDirection: "row", alignItems: "center" })} >
+      <View style={applyStyle({ backgroundColor: 'white', paddingTop: LibStyle.STATUSBAR_HEIGHT, borderBottomWidth: 0.5, borderBottomColor: "#ddd", flexDirection: "row", alignItems: "center" })} >
         <Pressable onPress={() => LibNavigation.back()} style={applyStyle({ alignItems: "center", justifyContent: "center", height: 50, width: 50 })} >
           <LibIcon name={"arrow-left"} />
         </Pressable>
@@ -58,23 +61,21 @@ export default function m(props: LogDetailProps): any {
         </View>
       </View>
       <View style={{ flex: 1 }}>
-        <LibScroll style={{ backgroundColor: '#2c3e50' }}>
+        <LibScroll>
           <View style={{ marginHorizontal: 15 }}>
             <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-              <Text allowFontScaling={false} selectable style={{ lineHeight: 16, fontSize: 12, fontFamily: 'mono', color: 'cyan' }} >{String(JSON.stringify(data || {}, undefined, 2))}</Text>
+              <Text allowFontScaling={false} selectable style={{ lineHeight: 18, fontSize: 12, fontFamily: 'mono', color: 'cyan' }} >{String(JSON.stringify(data || {}, undefined, 2))}</Text>
             </ScrollView>
           </View>
           <View />
         </LibScroll>
       </View>
-      {/* <View style={{ flexDirection: 'row', alignItems: 'center', marginHorizontal: 15, marginVertical: 10 }}>
-        <Pressable onPress={() => { }} style={{ flex: 1 }} >
-          <View style={[{ height: 40, backgroundColor: "#6c432c", flexDirection: 'row', alignItems: 'center', justifyContent: 'center', paddingHorizontal: 9 }]} >
-            <LibIcon name='send' size={16} color="white" />
-            <Text allowFontScaling={false} style={{ fontSize: 14, textAlign: "center", textAlignVertical: 'center', color: 'white', marginLeft: 10 }} >{'Kirim Telegram'}</Text>
-          </View>
-        </Pressable>
-      </View> */}
+      <Pressable onPress={() => {
+        sendToTelegram()
+      }} style={{ height: 40, backgroundColor: LibStyle.colorPrimary, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', borderRadius: 5, margin: 10 }} >
+        <LibIcon name='send' size={20} color="white" />
+        <Text allowFontScaling={false} style={{ fontSize: 14, textAlign: "center", textAlignVertical: 'center', color: 'white', marginLeft: 10 }} >{'send to esp dev-error'}</Text>
+      </Pressable>
     </View>
   )
 }
